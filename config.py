6a4f25ed400867eba9b543e3399a61a067c693d0
claudiahW <murungiclaud@gmail.com>
2021-11-06 22:57:57 +0300

    create user table

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://claudbae:12345@localhost/pitchperfect'
-    
+
 
 
 class ProdConfig(Config):
