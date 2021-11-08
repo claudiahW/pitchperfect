19bdf16bd6a9b4701a972b64145f011727efd0d9
claudiahW <murungiclaud@gmail.com>
2021-11-08 15:37:50 +0300

    create registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,6 +5,15 @@ from ..models import User
 from wtforms import ValidationError 
 
 class RegistrationForm(FlaskForm):
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+               
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
