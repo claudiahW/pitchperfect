a40529e23f2550f34c810823179e5d87a6fe2833
claudiahW <murungiclaud@gmail.com>
2021-11-08 15:32:54 +0300

    created authorization form

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,request,redirect,url_for
+from flask_login import login_required
 from . import main
 # from .forms import ReviewForm
 # from ..models import Review
@@ -6,6 +7,8 @@ from . import main
 # Views
 @main.route('/')
 def index():
+ @login_required
+ def new_review(id):
 
     '''
     View root page function that returns the index page and its data
@@ -13,3 +16,4 @@ def index():
 
     message = 'Hello World'
     return render_template('index.html',message = message)
+    
