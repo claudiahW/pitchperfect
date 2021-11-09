c4fa80da68c533f52fef7412d5e78cc32dbbb70a
claudiahW <murungiclaud@gmail.com>
2021-11-09 18:36:06 +0300

    initialize flask logins

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager 
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -11,7 +11,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
 
