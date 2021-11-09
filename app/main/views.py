f71edffcae26363f10e8a9c8b663fb5100fa585f
claudiahW <murungiclaud@gmail.com>
2021-11-09 18:44:04 +0300

    define user route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from flask_login import login_required
 from . import main
+from .. import db,photos
+
 # from .forms import ReviewForm
 # from ..models import Review
 
@@ -17,3 +19,13 @@ def index():
     message = 'Hello World'
     return render_template('index.html',message = message)
     
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
 
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://claudbae:12345@localhost/pitchperfect'
